1d81b2a552f793bf61d56c2e59b90d4e953434e6
Rudra <127481610+rudradalwadi@users.noreply.github.com>
2026-01-31 08:20:44 +0530

    Add files via upload

---
+++ ec2/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "disk_size" {
+  type = number
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+
+variable "key_name" {
+  type = string
+}
